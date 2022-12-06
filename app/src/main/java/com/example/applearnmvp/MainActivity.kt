409b573defd416d2a77183d79aa43dd90bcc1982
Evgeniy Alipatov <96283304+ealipatov@users.noreply.github.com>
2022-12-06 22:19:21 +0300

    Урок 1. Создали простое приложение с неправильным подходом без архитектуры (MVP в данном случае).

--- app/src/main/java/com/example/applearnmvp/MainActivity.kt
+++ app/src/main/java/com/example/applearnmvp/MainActivity.kt
@@ -1,21 +1,58 @@
 package com.example.applearnmvp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.applearnmvp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var binding: ActivityMainBinding? = null
+    private lateinit var binding: ActivityMainBinding
+
+    private val counters = mutableListOf(0, 0, 0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding?.root)
+        setContentView(binding.root)
+
+        initViews()
+
+        with(binding){
+            btnOne.setOnClickListener {
+                textViewOne.text = (++counters[0]).toString()
+            }
+            btnTwo.setOnClickListener {
+                textViewTwo.text = (++counters[1]).toString()
+            }
+            btnThree.setOnClickListener {
+                textViewThree.text = (++counters[2]).toString()
+            }
+        }
     }
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
+    private fun initViews() {
+        with(binding){
+            textViewOne.text = counters[0].toString()
+            textViewTwo.text = counters[1].toString()
+            textViewThree.text = counters[2].toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putIntArray("count", counters.toIntArray())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val array = savedInstanceState.getIntArray("count")
+        counters.let{list ->
+            list.clear()
+            array?.toList()?.let{
+                list.addAll(it)
+            }
+        }
+        initViews()
+    }
+
 }
